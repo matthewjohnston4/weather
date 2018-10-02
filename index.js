8fe6c85989a0aa36f2d958a09780eec69e5b24c1
Matthew Johnston <mjohnston@slack-corp.com>
2018-10-02 16:31:40 +0100

    Move data to API, build React skeleton

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import gql from "graphql-tag";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider, Query } from "react-apollo";
 
-class HelloMessage extends React.Component {
-  render() {
-    return <div>Hello {this.props.name}</div>;
-  }
-}
+const client = new ApolloClient({
+  uri: "https://weather-api.thedeskofmatthew.com/graphql"
+});
 
-var mountNode = document.getElementById("app");
-ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
+
+const TimeTicker = () => (
+  <div className="timeTicker"></div>
+);
+
+const ModelBlock = (props) => (
+  <div className="modelBlock">{JSON.stringify(props.modelData)}</div>
+);
+
+const ModelList = () => (
+  <Query
+    query={gql`
+      {
+        models {
+          name
+          res
+          minFrameSize
+          runs {
+            label
+          }
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return data.models.map((modelData) => (
+        <ModelBlock modelData={modelData} />
+      ));
+    }}
+  </Query>
+);
+
+const App = () =>  (
+  <ApolloProvider client={client}>
+    <TimeTicker />
+    <div className="modelList">
+      <ModelList />
+    </div>
+  </ApolloProvider>
+);
+
+render(<App name="Jane" />, document.getElementById("app"));
